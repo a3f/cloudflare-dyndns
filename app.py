d34e16521fa97493baad025d2142173820b78056
Ahmad Fatoum <ahmad@a3f.at>
2024-02-07 21:55:45 +0100

    from flask import request, jsonify into current namespace
    We use this at multiple locations and will add more references in a
    follow-up commit, so import them into the current namespace, so we don't
    have to write flask.request everywhere.

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import os
 import CloudFlare
 import waitress
-import flask
+from flask import Flask, request, jsonify
 import socket
 import requests
 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def main():
-    token = flask.request.args.get('token')
-    zone = flask.request.args.get('zone')
-    record = flask.request.args.get('record')
-    ipv4 = flask.request.args.get('ipv4')
-    ipv6 = flask.request.args.get('ipv6')
+    token = request.args.get('token')
+    zone = request.args.get('zone')
+    record = request.args.get('record')
+    ipv4 = request.args.get('ipv4')
+    ipv6 = request.args.get('ipv6')
 
     if not token:
         try:
@@ -25,18 +25,18 @@ def main():
             pass
 
     if not token:
-        return flask.jsonify({'status': 'error', 'message': 'Missing token URL parameter.'}), 400
+        return jsonify({'status': 'error', 'message': 'Missing token URL parameter.'}), 400
     if not zone:
-        return flask.jsonify({'status': 'error', 'message': 'Missing zone URL parameter.'}), 400
+        return jsonify({'status': 'error', 'message': 'Missing zone URL parameter.'}), 400
     if not ipv4 and not ipv6:
-        return flask.jsonify({'status': 'error', 'message': 'Missing ipv4 or ipv6 URL parameter.'}), 400
+        return jsonify({'status': 'error', 'message': 'Missing ipv4 or ipv6 URL parameter.'}), 400
 
     cf = CloudFlare.CloudFlare(token=token)
     try:
         zones = cf.zones.get(params={'name': zone})
 
         if not zones:
-            return flask.jsonify({'status': 'error', 'message': 'Zone {} does not exist.'.format(zone)}), 404
+            return jsonify({'status': 'error', 'message': 'Zone {} does not exist.'.format(zone)}), 404
 
         record_zone_concat = '{}.{}'.format(record, zone) if record is not None else zone
 
@@ -46,10 +46,10 @@ def main():
                                             'name': record_zone_concat, 'match': 'all', 'type': 'AAAA'})
 
         if ipv4 is not None and not a_record:
-            return flask.jsonify({'status': 'error', 'message': f'A record for {record_zone_concat} does not exist.'}), 404
+            return jsonify({'status': 'error', 'message': f'A record for {record_zone_concat} does not exist.'}), 404
 
         if ipv6 is not None and not aaaa_record:
-            return flask.jsonify({'status': 'error', 'message': f'AAAA record for {record_zone_concat} does not exist.'}), 404
+            return jsonify({'status': 'error', 'message': f'AAAA record for {record_zone_concat} does not exist.'}), 404
 
         if ipv4 is not None and a_record[0]['content'] != ipv4:
             cf.zones.dns_records.put(zones[0]['id'], a_record[0]['id'], data={
@@ -59,29 +59,29 @@ def main():
             cf.zones.dns_records.put(zones[0]['id'], aaaa_record[0]['id'], data={
                                      'name': aaaa_record[0]['name'], 'type': 'AAAA', 'content': ipv6, 'proxied': aaaa_record[0]['proxied'], 'ttl': aaaa_record[0]['ttl']})
     except CloudFlare.exceptions.CloudFlareAPIError as e:
-        return flask.jsonify({'status': 'error', 'message': str(e)}), 500
+        return jsonify({'status': 'error', 'message': str(e)}), 500
 
-    return flask.jsonify({'status': 'success', 'message': 'Update successful.'}), 200
+    return jsonify({'status': 'success', 'message': 'Update successful.'}), 200
 
 def inconsistent_ip(host, actual, expect):
-    return flask.jsonify({'status': 'error',
+    return jsonify({'status': 'error',
                           'host': host,
                           'message': 'inconsistent IP addresses',
                           'addrs': { "actual": actual, "expect": expect }}), 500
 
 @app.route('/healthz', methods=['GET'])
 def healthz():
-    ipv4 = flask.request.args.get('ipv4')
-    ipv6 = flask.request.args.get('ipv6')
+    ipv4 = request.args.get('ipv4')
+    ipv6 = request.args.get('ipv6')
 
     if ipv4 is None and ipv6 is None:
-        return flask.jsonify({'status': 'success', 'message': 'OK'}), 200
+        return jsonify({'status': 'success', 'message': 'OK'}), 200
 
-    zone = flask.request.args.get('zone')
-    record = flask.request.args.get('record')
+    zone = request.args.get('zone')
+    record = request.args.get('record')
 
     if not zone:
-        return flask.jsonify({'status': 'error', 'message': 'Missing zone URL parameter.'}), 400
+        return jsonify({'status': 'error', 'message': 'Missing zone URL parameter.'}), 400
 
     record_zone_concat = '{}.{}'.format(record, zone) if record is not None else zone
 
@@ -94,7 +94,7 @@ def healthz():
     try:
         data = socket.getaddrinfo(record_zone_concat, None)
     except socket.gaierror as e:
-        return flask.jsonify({'status': 'error', 'message': e.args[1]}), 500
+        return jsonify({'status': 'error', 'message': e.args[1]}), 500
 
     got_ipv4 = got_ipv6 = False
 
@@ -123,7 +123,7 @@ def healthz():
     if ipv6:
         response['ipv6'] = ipv6
 
-    return flask.jsonify(response), 200
+    return jsonify(response), 200
 
 app.secret_key = os.urandom(24)
 waitress.serve(app, host='0.0.0.0', port=80)
